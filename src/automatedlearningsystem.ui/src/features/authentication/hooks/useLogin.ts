4032035f1ddb32489782055a4b07cc3d7d849547
Syed Junaid <sjunaiduk@gmail.com>
2024-02-27 16:50:36 +0000

    removed redundant navigation is useLogin hook since loginPage subscribes to the user store anyway

--- src/automatedlearningsystem.ui/src/features/authentication/hooks/useLogin.ts
+++ src/automatedlearningsystem.ui/src/features/authentication/hooks/useLogin.ts
@@ -1,17 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import authService from "../services/authService";
-import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../stores/userStore";
 
 export const useLogin = ({ Email, Password }: LoginData) => {
-  const navigate = useNavigate();
   const { LoginUser } = useAuthStore();
   const { mutate } = useMutation({
     mutationFn: () => authService.Login({ Email, Password }),
     onSuccess: (loginResponse) => {
       console.log("Login success ", loginResponse);
       LoginUser(loginResponse);
-      navigate("/");
     },
     onError: () => console.log("Login failed"),
   });
